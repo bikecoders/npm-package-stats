480dee7f4fba6c2f5feb37361b75ace3d10f9d5d
dianjuar <dianjuar@gmail.com>
2019-01-26 18:06:00 -0500

    Implement health check endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealthCheck(): any {
+    return {
+      status: true,
+      message: 'I\'m ok, thank you',
+    };
   }
 }
