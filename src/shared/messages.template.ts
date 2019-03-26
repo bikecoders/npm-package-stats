dda66770480e6e591ec659687ad8f7090d9e79f0
dianjuar <dianjuar@gmail.com>
2019-03-25 22:03:21 -0500

    feat: improve /start command

---
+++ src/shared/messages.template.ts
@@ -0,0 +1,6 @@
+export const welcome =
+`Welcome 👋
+Here you check your NPM packages stats.
+
+Try out with \`/add your-package-slug\`
+😸`;
--- src/telegram-bot/telegram-bot.service.ts
+++ src/telegram-bot/telegram-bot.service.ts
@@ -1,52 +1,51 @@
 import { Injectable } from '@nestjs/common';
 
-import { of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-
 import * as TelegramBot from 'node-telegram-bot-api';
-import { User } from 'src/models/user.interface';
-import { UsersService } from 'src/users/users.service';
+
+import { User } from '../models/user.interface';
+import { UsersService } from '../users/users.service';
+import * as Messages from '../shared/messages.template';
 
 @Injectable()
 export class TelegramBotService {
-  constructor(userService: UsersService) {
+  /**
+   * Telegram bot instance
+   */
+  bot: TelegramBot;
 
+  constructor(private userService: UsersService) {
     // replace the value below with the Telegram token you receive from @BotFather
     const token = process.env.TELEGRAM_BOT_KEY;
 
     // Create a bot that uses 'polling' to fetch new updates
-    const bot = new TelegramBot(token, { polling: true });
+    this.bot = new TelegramBot(token, { polling: true });
 
-    // Matches "/echo [whatever]"
-    bot.onText(/\/echo (.+)/, (msg, match) => {
-      // 'msg' is the received Message from Telegram
-      // 'match' is the result of executing the regexp above on the text content
-      // of the message
+    this.createCommandListeners();
+  }
 
+  /**
+   * Create listeners for all the supported commands
+   */
+  private createCommandListeners() {
+    this.commandListenerStart();
+  }
+
+  /**
+   * /start command listener
+   */
+  private commandListenerStart() {
+    this.bot.onText(/\/start/, (msg, match) => {
       const chatId = msg.chat.id;
-      const resp = match[1]; // the captured "whatever"
+      const user: User = { chatId: chatId.toString() };
 
-      // send back the matched "whatever" to the chat
-      bot.sendMessage(chatId, resp);
-    });
-
-    // Listen for any kind of message. There are different kinds of
-    // messages.
-    bot.on('message', (msg) => {
-      const chatId = msg.chat.id;
-
-      // send a message to the chat acknowledging receipt of their message
-      bot.sendMessage(chatId, 'Received your message');
-    });
-
-    bot.onText(/\/start/, (msg, match) => {
-      const chatId = msg.chat.id;
-
-      // TODO: catch exception on duplicated user
-      userService.create({ chatId: chatId.toString() })
-        .subscribe(userCreated => {
-          bot.sendMessage(chatId, `${userCreated.chatId} - User created`);
+      this.userService.create(user)
+        .subscribe(_ => {
+          this.sendMessageMarkdown(chatId, Messages.welcome);
         });
     });
+  }
+
+  private sendMessageMarkdown(chatId: string | number, message: string) {
+    this.bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
   }
 }
