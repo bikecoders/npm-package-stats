73e2fcb9410892de57324cc437a19b70c02800b3
dianjuar <dianjuar@gmail.com>
2019-03-25 19:55:43 -0500

    feat: add unit test user service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { User } from '../models/user.interface';
+
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
+
+jest.mock('./users.repository');
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepo: UsersRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        UsersRepository,
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepo = module.get<UsersRepository>(UsersRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Create User', () => {
+    it('should create an user', () => {
+      const userToCreate: User = {
+        chatId: '12345',
+      };
+
+      service.create(userToCreate);
+
+      expect(userRepo.create).toHaveBeenCalledWith(userToCreate);
+    });
   });
 });
