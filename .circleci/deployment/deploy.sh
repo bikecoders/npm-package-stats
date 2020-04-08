9e08992fb151373304451f44fa960f6bdd35ac5a
Diego Juliao <dianjuar@gmail.com>
2020-04-07 22:47:08 -0500

    refactor: migrate CD to another server

--- .circleci/deployment/deploy.sh
+++ .circleci/deployment/deploy.sh
@@ -4,11 +4,9 @@ log_prefix=" --- ";
 function restart_everything() {
   echo "$log_prefix Restarting everything";
   # stop the program
-  ssh $remote_server_ip -l $remote_server_user -p $remote_server_port \
-    "killall node";
+  execute_remote_command "cd $remote_server_path && docker-compose down";
   # Remove all files but the database directory
-  ssh $remote_server_ip -l $remote_server_user -p $remote_server_port \
-    "cd $remote_server_path && ls . | grep -v 'database/*' | xargs rm -R";
+  execute_remote_command "cd $remote_server_path && ls . | grep -v 'database/*' | xargs rm -R";
 }
 
 function generate_files() {
@@ -21,32 +19,46 @@ function generate_files() {
 function copy_files() {
   echo "$log_prefix Copying files";
 
-  tar -cvf files-to-copy.tar \
-    ./package.json \
-    ./yarn.lock \
-    ./dist \
-    ./src/environments/.envProd
+  temporaryFolder='files-to-copy'
+
+  mkdir $temporaryFolder
+
+  # Pile all the files
+  cp ./package.json ./$temporaryFolder/package.json
+  cp ./yarn.lock ./$temporaryFolder/yarn.lock
+  cp -r ./dist ./$temporaryFolder/dist
+  cp ./src/environments/.envProd ./$temporaryFolder/.envProd
+  cp ./.circleci/deployment/docker-compose.yml ./$temporaryFolder/docker-compose.yml
+
+  cd $temporaryFolder
+  tar -cvf files-to-copy.tar .
+  mv files-to-copy.tar ..
+  cd ..
 
   # Copy files
   scp -P $remote_server_port files-to-copy.tar $remote_server_user@$remote_server_ip:$remote_server_path
   # Unzip them and delete tar
-  ssh $remote_server_ip -l $remote_server_user -p $remote_server_port \
-    "cd $remote_server_path && tar -xvf files-to-copy.tar && rm files-to-copy.tar";
+  execute_remote_command "cd $remote_server_path && tar -xf files-to-copy.tar && rm files-to-copy.tar";
 
-  # Delete local tar
+  # Delete local files
   rm files-to-copy.tar;
+  rm -R $temporaryFolder;
+}
+
+
+function update_docker_images() {
+  echo "$log_prefix Updating Docker Images";
+  execute_remote_command "cd $remote_server_path && docker-compose pull";
 }
 
 function install_dependencies() {
   echo "$log_prefix Installing dependencies";
-  ssh $remote_server_ip -l $remote_server_user -p $remote_server_port \
-    "cd $remote_server_path && yarn install --production=true";
+  execute_remote_command "cd $remote_server_path && docker-compose run --rm server yarn install --production=true";
 }
 
 function start_project() {
   echo "$log_prefix Starting project";
-  ssh $remote_server_ip -l $remote_server_user -p $remote_server_port \
-    "cd $remote_server_path && npx cross-env $(cat ./src/environments/.envProd | xargs) node ./dist/main.js >> ./database/log.txt" > /dev/null &
+  execute_remote_command "cd $remote_server_path && docker-compose up -d";
 }
 
 # Check if the API is up doing a ping maximum 10 times
@@ -54,12 +66,11 @@ function check_api_is_alive() {
   echo "$log_prefix Checking health of API";
   api_alive=false;
 
-  for i in `seq 1 20`;
+  for i in `seq 1 10`;
   do
     echo "Try number $i";
     # Make the "ping" in silence
-    ssh $remote_server_ip -l $remote_server_user -p $remote_server_port \
-      "curl localhost:3000/health &> /dev/null;"
+    curl $remote_server_ip:3000/health &> /dev/null;
 
     if [ $? -eq 0 ]; then
       api_alive=true;
@@ -81,9 +92,14 @@ function exit_indicating_status() {
   fi
 }
 
+function execute_remote_command() {
+  ssh $remote_server_ip -l $remote_server_user -p $remote_server_port $1;
+}
+
 restart_everything;
 generate_files;
 copy_files;
+update_docker_images;
 install_dependencies;
 start_project;
 check_api_is_alive;
